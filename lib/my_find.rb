7d2165f624b1d55e14b77934664e936f613f7f79
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 23:52:18 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,10 @@ require 'pry'
 def my_find(collection)
   if !(collection.empty?)
     counter = 0
-    collection.each do |x|
-      counter += 1
-      
+    while counter < collection.length
+      collection.each do |x|
+        counter += 1
+      end  
     end
     collection[counter]
   end  
